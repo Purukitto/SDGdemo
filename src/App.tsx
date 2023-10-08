880d7b42e1b670e96117f15d3d7c72b42b1f54e4
Purukitto <purukitto@gmail.com>
2023-10-08 22:55:42 +0530

    Base project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-	return <div className="text-3xl">Hello</div>;
+	return (
+		<div className="bg-black h-screen flex text-white">
+			<div className="m-auto">
+				<h1 className="text-4xl">Hello World</h1>
+			</div>
+		</div>
+	);
 }
 
 export default App;
