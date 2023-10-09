9fe3b9ce537600e70b304d8153205d42de1946ae
Purukitto <purukitto@gmail.com>
2023-10-09 20:34:44 +0530

    Updated view and fixed build relative paths

--- src/App.jsx
+++ src/App.jsx
@@ -174,7 +174,7 @@ function App() {
 						>
 							simpledungeongenerator
 						</a>
-						<span className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">
+						<span className="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300">
 							{packageJson.dependencies.simpledungeongenerator}
 						</span>
 					</h1>
