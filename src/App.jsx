a19dabdc591425c872707f3fd13e11d77c3b4b54
Purukitto <purukitto@gmail.com>
2023-10-09 15:25:10 +0530

    Update for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -174,7 +174,7 @@ function App() {
 						>
 							simpledungeongenerator
 						</a>
-						<span class="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">
+						<span className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">
 							{packageJson.dependencies.simpledungeongenerator}
 						</span>
 					</h1>
